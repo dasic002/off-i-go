67bbfb0a5d3fd0921ebb8725d4246cd48446fbda
dasic002 <dasic002@yahoo.co.uk>
2025-03-24 17:05:57 +0000

    Add posts app, post serializer and list view

---
+++ posts/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    body = models.TextField(blank=True)
+    listing_type = models.IntegerField(
+        choices=[
+            (0, 'Draft'),
+            (1, 'Private'),
+            (2, 'Unlisted'),
+            (3, 'Public')
+        ], default=0
+    )
+    original_post = models.ForeignKey(
+        'Post',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='reposts'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.id}: {self.title}"
---
+++ posts/views.py
@@ -0,0 +1,46 @@
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostList(APIView):
+    """
+    List all posts or create a new post.
+    """
+    serializer_class = PostSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(
+            posts, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+    
+# class PostDetail(APIView):
+#     serializer_class = PostSerializer
+#     permission_classes = [IsOwnerOrReadOnly]
+#     def get_object(self, pk):
+#         try:
+#             post = Post.objects.get(pk=pk)
+#             self.check_object_permissions(self.request, post)
+#             return post
+#         except Post.DoesNotExist:
+#             raise Http404
