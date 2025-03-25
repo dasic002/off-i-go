3466a4f9e2fe10f04d726fda34fb38ce0e136c4e
dasic002 <dasic002@yahoo.co.uk>
2025-03-25 10:00:47 +0000

    Add Comment replies data to view

--- comments/models.py
+++ comments/models.py
@@ -13,9 +13,15 @@ class Comment(models.Model):
     )
     post = models.ForeignKey(
         Post,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name='comments'
     )
     body = models.TextField()
+    # is the best way to handle replies to comments just letting react select a
+    # comment id that has the same post id?
+    # then on the backend, we check that if a reply_to is not None, then we
+    # check it is valid by checking the original comment's post id matches that
+    # of the reply comment?
     reply_to = models.ForeignKey(
         'Comment',
         on_delete=models.CASCADE,
@@ -31,3 +37,32 @@ class Comment(models.Model):
 
     def __str__(self):
         return {self.body}
+    
+    # def get_post_comments(self):
+    #     return self.post.comments.all()
+
+
+# class CommentReply(Comment):
+#     """
+#     Model for replies to comments
+#     """
+#     reply_to = models.ForeignKey(
+#         Comment.objects.filter(post=post),
+#         on_delete=models.CASCADE,
+#         related_name='replies'
+#     )
+
+#     class Meta:
+#         ordering = ['-created_at']
+
+#     def __str__(self):
+#         return {self.body}
+    
+
+    # models.ForeignKey(
+    #     'self'.objects.filter(post=post),
+    #     on_delete=models.CASCADE,
+    #     blank=True,
+    #     null=True,
+    #     related_name='replies'
+    # )
--- comments/serializers.py
+++ comments/serializers.py
@@ -10,16 +10,22 @@ class CommentSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    reply_to = serializers.ReadOnlyField(source='reply_to.id')
+    replies = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
     
+    def get_replies(self, obj):
+        replies = obj.replies.all()
+        return CommentSerializer(replies, many=True).data
+
     class Meta:
         model = Comment
         fields = [
             'id', 'owner', 'profile_id', 'profile_image', 'post', 'body',
-            'created_at', 'updated_at', 'is_owner'
+            'created_at', 'updated_at', 'is_owner', 'reply_to', 'replies'
         ]
 
 
