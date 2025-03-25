b4d2d70828c59831680d2de8addd18a0f68d2b72
dasic002 <dasic002@yahoo.co.uk>
2025-03-25 18:31:20 +0000

    Add logout view to overcome known bug

--- off_i_go/views.py
+++ off_i_go/views.py
@@ -1,5 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .settings import(
+    JWT_AUTH_COOKIE,
+    JWT_AUTH_REFRESH_COOKIE,
+    JWT_AUTH_SAMESITE,
+    JWT_AUTH_SECURE,
+)
 
 
 @api_view()
@@ -7,3 +13,36 @@ def root_route(request):
     return Response({
         "message": "Welcome to Off I Go API",
     })
+
+
+# dj-rest-auth logout view fix
+@api_view(['POST'])
+def logout_route(request):
+    response = Response()
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE
+    )
+    return response
+
+# dj-rest-auth logout view fix suggested by CoPilot
+# @api_view(['POST'])
+# def logout_route(request):
+#     response = Response()
+#     response.delete_cookie(JWT_AUTH_COOKIE, samesite=JWT_AUTH_SAMESITE, secure=JWT_AUTH_SECURE)
+#     response.delete_cookie(JWT_AUTH_REFRESH_COOKIE, samesite=JWT_AUTH_SAMESITE, secure=JWT_AUTH_SECURE)
+#     return response
