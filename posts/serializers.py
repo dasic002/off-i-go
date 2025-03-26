021369d379ca0caccc8ebf658d7d57fc1aa48c12
dasic002 <dasic002@yahoo.co.uk>
2025-03-26 10:06:30 +0000

    Add fields to Posts for user reactions

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from reactions.models import Reaction
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,6 +8,30 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    reaction_id = serializers.SerializerMethodField()
+    reaction_type_id = serializers.SerializerMethodField()
+    reaction_type = serializers.SerializerMethodField()
+
+    def get_reaction_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            reaction = obj.reactions.filter(owner=user).first()
+            return reaction.id if reaction else None
+        return None
+    
+    def get_reaction_type_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            reaction = obj.reactions.filter(owner=user).first()
+            return reaction.reaction if reaction else None
+        return None
+    
+    def get_reaction_type(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            reaction = obj.reactions.filter(owner=user).first()
+            return reaction.get_reaction_display() if reaction else None
+        return None
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -17,5 +42,6 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'profile_id', 'profile_image', 'title', 'body',
             'listing_type', 'original_post', 'created_at', 'updated_at',
-            'is_owner'
+            'is_owner', 'reaction_id', 'reaction_type_id',
+            'reaction_type'
         ]
