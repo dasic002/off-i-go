581d00230462e665f1fc4bd92e6c6d946a410157
dasic002 <dasic002@yahoo.co.uk>
2025-03-31 13:57:29 +0100

    Fix display as main route to display the react app

--- off_i_go/urls.py
+++ off_i_go/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from .views import root_route, logout_route
+from .views import logout_route
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
-    path('api/', root_route),
     path('api/api-auth/', include('rest_framework.urls')),
     # the logout route has to be above the default route to be matched first
     path('api/dj-rest-auth/logout/', logout_route),
