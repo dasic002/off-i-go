09e6e2d7e80ebacbce6c59bca39c166b7a2eafb5
dasic002 <dasic002@yahoo.co.uk>
2025-03-31 20:01:18 +0100

    Add top navbar, favicon and stylings
    Fix react-bootstrap install under frontend directory

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <Navbar expand="md" fixed="top" className={styles.NavBar}>
+      <Container>
+        <NavLink to="/">
+          <Navbar.Brand className={styles.Brand}>Off I Go</Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-right">
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fas fa-home"></i>Home
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signup"
+            >
+              <i className="fas fa-user-plus"></i>Sign up
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
