6b9406cca0d44e1e121f79706ee727b0db1096ad
dasic002 <dasic002@yahoo.co.uk>
2025-04-10 21:47:33 +0100

    Add function to follow and unfollow profiles

--- frontend/src/contexts/ProfileDataContext.js
+++ frontend/src/contexts/ProfileDataContext.js
@@ -1,6 +1,7 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { axiosReq } from "../api/axiosDefaults";
+import { axiosRes } from "../api/axiosDefaults";
 import { useCurrentUser } from "./CurrentUserContext";
+import { followHelper, unfollowHelper } from "../utils/utils";
 
 export const ProfileDataContext = createContext();
 export const SetProfileDataContext = createContext();
@@ -17,10 +18,58 @@ export const ProfileDataProvider = ({ children }) => {
 
   const currentUser = useCurrentUser();
 
+  const handleFollow = async (clickedProfile) => {
+    try {
+      const { data } = await axiosRes.post("/followers/", {
+        followed: clickedProfile.id,
+      });
+
+      setProfileData((prevState) => ({
+        ...prevState,
+        pageProfile: {
+          results: prevState.pageProfile.results.map((profile) =>
+            followHelper(profile, clickedProfile, data.id)
+          ),
+        },
+        popularProfiles: {
+          ...prevState.popularProfiles,
+          results: prevState.popularProfiles.results.map((profile) =>
+            followHelper(profile, clickedProfile, data.id)
+          ),
+        },
+      }));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleUnfollow = async (clickedProfile) => {
+    try {
+      await axiosRes.delete(`/followers/${clickedProfile.following_id}/`);
+
+      setProfileData((prevState) => ({
+        ...prevState,
+        pageProfile: {
+          results: prevState.pageProfile.results.map((profile) =>
+            unfollowHelper(profile, clickedProfile)
+          ),
+        },
+        popularProfiles: {
+          ...prevState.popularProfiles,
+          results: prevState.popularProfiles.results.map((profile) =>
+            unfollowHelper(profile, clickedProfile)
+          ),
+        },
+      }));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const { data } = await axiosReq.get(
+        const { data } = await axiosRes.get(
           "/profiles/?ordering=-followers_count"
         );
         setProfileData((prevState) => ({
@@ -37,7 +86,9 @@ export const ProfileDataProvider = ({ children }) => {
 
   return (
     <ProfileDataContext.Provider value={profileData}>
-      <SetProfileDataContext.Provider value={setProfileData}>
+      <SetProfileDataContext.Provider
+        value={{ setProfileData, handleFollow, handleUnfollow }}
+      >
         {children}
       </SetProfileDataContext.Provider>
     </ProfileDataContext.Provider>
--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -16,3 +16,33 @@ export const fetchMoreData = async (resource, setResource) => {
     console.error(err);
   }
 };
+
+export const followHelper = (profile, clickedProfile, following_id) => {
+  return profile.id === clickedProfile.id
+    ? // profile I clicked on, update its folowers count and set its following id
+      {
+        ...profile,
+        followers_count: profile.followers_count + 1,
+        following_id,
+      }
+    : profile.is_owner
+    ? // this is the profile of the logged in user update their following count
+      { ...profile, following_count: profile.following_count + 1 }
+    : // this is neither the profile the user clicked to follow or the logged in user, return it as is
+      profile;
+};
+
+export const unfollowHelper = (profile, clickedProfile) => {
+  return profile.id === clickedProfile.id
+    ? {
+        // this profile is the clicked profile, update its followers count and set its following id to null
+        ...profile,
+        followers_count: profile.followers_count - 1,
+        following_id: null,
+      }
+    : profile.is_owner
+    ? // if the profile is the logged in user, update their following count
+      { ...profile, following_count: profile.following_count - 1 }
+    : // if neither, return current profile data as is
+      profile;
+};
